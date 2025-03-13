8f2c47f7a0f54ae3a5caf9e1bfede8acea90dc1c
DanMazzaro <danielmazzaro44@gmail.com>
2025-03-13 09:05:49 -0300

    Add files via upload

---
+++ exercicio 9.py
@@ -0,0 +1,11 @@
+nota = int(input('qual a nota? '))
+if nota >= 90 and nota <= 100:
+    print('nota A')
+elif nota >= 80 and nota <= 89:
+    print('nota B')
+if nota >= 70 and nota <= 79:
+    print('nota C')
+if nota >= 60 and nota <= 69:
+    print('nota D')
+if nota >= 0 and nota <= 59:
+    print('nota F')
